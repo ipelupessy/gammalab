79c1cd42d36e8071f207a319442ab019ecfc7b53
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-07-18 09:42:45 +0200

    fix package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='GammaLab',
@@ -6,22 +6,23 @@ setup(
     description='Toolkit for soundcard gamma ray spectroscopy',
     author='F.I. Pelupessy',
     url='https://github.com/ipelupessy/gammalab',
-    license='GPL',
-    packages=['gammalab'],
+    license='GPLv3',
+    packages=find_packages('src'),
     package_data={},
+    package_dir={'' : 'src'},
     install_requires=['numpy','pyaudio'],
     python_requires='>=2.7',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GPL',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3) ',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: High Energy Physics',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     long_description=open('README.rst').read(),
 )
