e11e50d26bfca966817d6a0ab4a746f90e88dcff
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-07-22 09:32:42 +0200

    timed mode for main

--- src/gammalab/main.py
+++ src/gammalab/main.py
@@ -1,14 +1,36 @@
 from . import all_services
 
-def main():
-    print(all_services)
+from threading import Timer
+import time
+
+def startup():
+    #~ print(all_services)
+    print("startup")
 
     for s in all_services:
         s.start()
-        
-    print("running")
-    raw_input()
-    
+
+def shutdown():
+    print("shutting down")
+
     for s in all_services:
         s.stop()
         s.close()
+        
+    exit(0)
+
+def main(timeout=None):
+
+    startup()
+
+    if timeout is not None:
+        print "timer"
+        timer=Timer(timeout, shutdown)
+        timer.start()
+        
+    print("running")
+    raw_input()
+
+    shutdown()
+    
+    
