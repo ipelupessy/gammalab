6230cc40789a00a0982ed514e9702b004f69bb8e
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-07-23 21:12:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GammaLab',
-    version='0.0.2',
+    version='0.1.0',
     description='Toolkit for soundcard gamma ray spectroscopy',
     author='F.I. Pelupessy',
     url='https://github.com/ipelupessy/gammalab',
