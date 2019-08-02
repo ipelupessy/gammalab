ebe4d5dc5da7f2c463cec15a413fdc0304a7fd94
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-08-02 17:43:38 +0200

    fix play back for python 3 and add argument for output device index

--- src/gammalab/backend/playback.py
+++ src/gammalab/backend/playback.py
@@ -7,18 +7,19 @@ pyaudio_format=dict(float32=pyaudio.paFloat32)
 pyaudio_nbytes=dict(float32=4)
 
 class Playback(ReceivingService):
-    def __init__(self, frames_per_buffer=2048):
+    def __init__(self, frames_per_buffer=2048, output_device_index=None):
         ReceivingService.__init__(self)
         self.input_wire=RawWire()      
         self.pyaudio=pyaudio.PyAudio()
         self.player=None
         self.frames_per_buffer=frames_per_buffer
+        self.output_device_index=output_device_index
 
     def _callback(self, in_data, frame_count, time_info, status):
         data=self.receive_input(block=False)
         if data is None:
             self.print_message("playback buffer underrun")
-            data=bytes(0)*pyaudio_nbytes[self.input_wire.FORMAT]*frame_count
+            data=bytes(bytearray(pyaudio_nbytes[self.input_wire.FORMAT]*frame_count))
         return (data, pyaudio.paContinue)
 
     def start(self):
@@ -28,7 +29,8 @@ class Playback(ReceivingService):
                 rate=self.input_wire.RATE,
                 frames_per_buffer=self.frames_per_buffer,
                 output=True,
-                stream_callback=self._callback
+                stream_callback=self._callback,
+                output_device_index=self.output_device_index
                 )
                 
     def stop(self):
