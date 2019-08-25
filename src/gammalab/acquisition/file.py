ea04a924ba10520fb770dd7be4e53c47a83309cb
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-08-25 15:57:09 +0200

    add option to replay in realtime

--- src/gammalab/acquisition/file.py
+++ src/gammalab/acquisition/file.py
@@ -7,7 +7,7 @@ import numpy
 pyaudio_nbytes=dict(float32=4)
 
 class RawReplay(ThreadService, SourceService):
-    def __init__(self, filename="data.raw", frames_per_buffer=1024):
+    def __init__(self, filename="data.raw", frames_per_buffer=1024, realtime=True):
         SourceService.__init__(self)
         ThreadService.__init__(self)
         self.CHANNELS=1
@@ -15,6 +15,7 @@ class RawReplay(ThreadService, SourceService):
         self.FORMAT="float32"
         self.filename=filename
         self.frames_per_buffer=frames_per_buffer
+        self.realtime=realtime
 
     def output_protocol(self, wire):
         assert isinstance(wire, RawWire)
@@ -23,11 +24,14 @@ class RawReplay(ThreadService, SourceService):
         wire.FORMAT=self.FORMAT
     
     def process(self, data=None):
-        time.sleep(self.frames_per_buffer/(1.*self.RATE))
-        t=time.time()
-        dt=t-self.t0
-        self.t0=t
-        n=int(dt*self.RATE/16)*16
+        if self.realtime:
+          time.sleep(self.frames_per_buffer/(1.*self.RATE))
+          t=time.time()
+          dt=t-self.t0
+          self.t0=t
+          n=int(dt*self.RATE/16)*16
+        else:
+          n=int(self.frames_per_buffer/16)*16
         try:
           data=self._file.read(pyaudio_nbytes[self.FORMAT]*n)
         except:
