e606149fc7c74fe292d22223d64063d5a39aeb54
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-12-23 14:21:27 +0100

    add protocol to wire

--- src/gammalab/wire.py
+++ src/gammalab/wire.py
@@ -3,18 +3,45 @@ try:
 except:
     from Queue import Queue
 
-class RawWire(Queue):
+class Wire(Queue):
+    _description="none"
+
+    def __init__(self, **kwargs):
+        super(Wire, self).__init__(**kwargs)
+        self.protocol=Dict()
+
+# attributes are forwarded to the protocol
+    def __getattr__(self, name):
+        try:
+            return getattr(super(Wire, self), name)
+        except AttributeError:
+            if name in self.protocol:
+                return self.protocol[name]
+            else:
+                raise AttributeError("No such attribute in protocol: " + name)
+
+    def __setattr__(self, name, value):
+        self.protocol[name] = value
+
+    def __delattr__(self, name):
+        if name in self.protocol:
+            del self.protocol[name]
+        else:
+            raise AttributeError("No such attribute in protocol: " + name)
+
+
+class RawWire(Wire):
     _description="Raw byte (string)"
 
-class FloatWire(Queue):
+class FloatWire(Wire):
     _description="numpy float array"
 
-class PulseWire(Queue):
+class PulseWire(Wire):
     _description="pulses"
 
-class MessageWire(Queue):
+class MessageWire(Wire):
     _description="output queue for terminal messages"
 
-class HistogramWire(Queue):
+class HistogramWire(Wire):
     _description="dict with histogram"
 
