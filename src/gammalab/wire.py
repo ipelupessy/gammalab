fe06bdb256af93c3847e348dc01d977c2940ec83
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-12-23 15:30:35 +0100

    attempt 2 of protocol for wire

--- src/gammalab/wire.py
+++ src/gammalab/wire.py
@@ -3,7 +3,35 @@ try:
 except:
     from Queue import Queue
 
-class RawWire(Queue):
+class Wire(Queue):
+    __initialized=False
+    _description="none"
+
+    def __init__(self, **kwargs):
+        Queue.__init__(self, **kwargs)
+        self.protocol=dict()
+        self.__initialized=True
+
+# attributes are forwarded to the protocol (after __init__)
+    def __setattr__(self, name, value):
+        if self.__initialized:
+            self.protocol[name] = value
+        else:
+            self.__dict__[name] = value
+
+    def __getattr__(self, name):
+        if name in self.protocol:
+            return self.protocol[name]
+        else:
+            raise AttributeError("No such attribute in protocol: " + name)
+
+    def __delattr__(self, name):
+        if name in self.protocol:
+            del self.protocol[name]
+        else:
+            raise AttributeError("No such attribute in protocol: " + name)
+
+class RawWire(Wire):
     _description="Raw byte (string)"
 
 class FloatWire(Queue):
