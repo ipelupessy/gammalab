eceb02051ffd7df0191bef95979cac3d2d760a20
ipelupessy <i.pelupessy@esciencecenter.nl>
2020-07-24 16:03:15 +0200

    rename pyaudio and soundcard modules (necessary for python 2.7)

--- src/gammalab/acquisition/__init__.py
+++ src/gammalab/acquisition/__init__.py
@@ -1,4 +1,4 @@
-from .pyaudio import PyAudio
-from .soundcard import SoundCard
+from ._pyaudio import PyAudio
+from ._soundcard import SoundCard
 from .generator import Noise
 from .file import RawReplay, WavReplay, FileReplay
