25fc5312eae8596a2de5c3b7351fc580fe176b0e
ipelupessy <i.pelupessy@esciencecenter.nl>
2020-07-24 15:42:22 +0200

    add soundcard acquisition

--- src/gammalab/acquisition/__init__.py
+++ src/gammalab/acquisition/__init__.py
@@ -1,3 +1,4 @@
 from .pyaudio import PyAudio
+from .soundcard import SoundCard
 from .generator import Noise
 from .file import RawReplay, WavReplay, FileReplay
---
+++ src/gammalab/acquisition/soundcard.py
@@ -0,0 +1,47 @@
+from ..service import SourceService, ThreadService
+from ..wire import RawWire
+
+import soundcard
+
+class SoundCard(ThreadService, SourceService):
+    def __init__(self, frames_per_buffer=2048, input_device_index=None, input_device_name="",
+                 sample_rate=48000, sample_format="float32"):
+        super(SoundCard, self).__init__()
+        self.CHANNELS=1
+        self.RATE=sample_rate
+        if sample_format != "float32":
+          raise Exception("SoundCard only supports float32 sample_format")
+        self.FORMAT=sample_format
+        self.recorder=None
+        self.frames_per_buffer=frames_per_buffer
+        self.input_device_index=input_device_index
+        self.input_device_name=input_device_name
+
+    def _process(self):
+        mic=soundcard.get_microphone(self.input_device_index or self.input_device_name)
+        with mic.recorder(samplerate=self.RATE, blocksize=self.frames_per_buffer, channels=self.CHANNELS) as recorder:
+            while not self.done:    
+                if not self.stopped and not self.done:
+                    try:
+                        data=recorder.record(self.frames_per_buffer)
+                    except:
+                        self.done=True
+                    if len(data)==0:
+                        self.print_message("no data")
+                        self.done=True
+                    else:
+                        data=data.astype("float32").tobytes()
+    
+                if (not self.stopped and 
+                    not self.done and
+                    data is not None):  
+                    self.send_output(data)
+                
+            self.send_output(None)
+        self.stopped=True
+
+    def output_protocol(self, wire):
+        assert isinstance(wire, RawWire)
+        wire.CHANNELS=self.CHANNELS
+        wire.RATE=self.RATE
+        wire.FORMAT=self.FORMAT
