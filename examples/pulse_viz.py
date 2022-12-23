6400e76d416652275ae8ac9793431dfea8c154f9
ipelupessy <i.pelupessy@esciencecenter.nl>
2022-12-23 14:23:14 +0100

    add examples, remove unused pulse plot version

---
+++ examples/pulse_viz.py
@@ -0,0 +1,20 @@
+from gammalab import main
+from gammalab.acquisition import SoundCard
+from gammalab.transform import Raw2Float
+from gammalab.analysis import PulseDetection
+from gammalab.analysis import Count
+from gammalab.backend import PulsePlot
+
+
+source=SoundCard(input_device_name="PCM2900")
+convert=Raw2Float()
+detect=PulseDetection(threshold=0.003, debug=True)
+count=Count()
+pulseplot=PulsePlot(nplot=5)
+
+source.plugs_into(convert)
+convert.plugs_into(detect)
+detect.plugs_into(count)
+detect.plugs_into(pulseplot)
+
+main()
